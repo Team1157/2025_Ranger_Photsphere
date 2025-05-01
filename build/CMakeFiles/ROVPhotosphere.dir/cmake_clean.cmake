21956e92c2dd7ebe9948eeedeb897d8fbca7d097
ada <cmdada@proton.me>
2025-05-01 14:37:12 -0600

    partial rewrite

---
+++ build/CMakeFiles/ROVPhotosphere.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ROVPhotosphere.dir/link.d"
+  "CMakeFiles/ROVPhotosphere.dir/main.cpp.o"
+  "CMakeFiles/ROVPhotosphere.dir/main.cpp.o.d"
+  "ROVPhotosphere"
+  "ROVPhotosphere.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ROVPhotosphere.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
